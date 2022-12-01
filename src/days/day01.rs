3d85c67fdd6343d38227a1f9600e447308f8cdbf
Shaun Karan <shaun.karran@polymathian.com>
2022-12-01 16:17:10 +1000

    Day 1 solution.

--- src/days/day01.rs
+++ src/days/day01.rs
@@ -4,9 +4,24 @@ use std::fs::read_to_string;
 ///////////////////////////////////////////////////////////////////////////////
 
 pub fn solve() -> SolutionPair {
-    // Your solution here...
-    let sol1: u64 = 0;
-    let sol2: u64 = 0;
+    let input: Vec<String> = read_to_string("input/day01.txt").unwrap()
+        .lines()
+        .map(str::to_string)
+        .collect();
 
-    (Solution::U64(sol1), Solution::U64(sol2))
+    let elves_str = input
+        .split(|line| line.is_empty());
+    
+    let mut elves: Vec<i32> = Vec::new();
+    for elf_str in elves_str {
+        let elf_ints: Vec<i32> = elf_str.into_iter().map(|x| x.parse().unwrap()).collect();
+        let elf_sum: i32 = elf_ints.iter().sum();
+        elves.push(elf_sum);
+    }
+    elves.sort_by(|a, b| b.partial_cmp(a).unwrap());
+
+    let sol1 = elves[0];
+    let sol2: i32 = elves.into_iter().take(3).reduce(|acc, x| acc + x).unwrap();
+
+    (Solution::I32(sol1), Solution::I32(sol2))
 }
